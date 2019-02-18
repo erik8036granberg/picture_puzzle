c273c3098b2b2ec90d3cfe9a26b744cf478e746a
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-18 11:48:28 +0100

    image loaded ok

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict";
+let inputSrc;
+let numOfXPieces;
+let numOfYPieces;
+const container_width = 400;
+const container_height = 400;
+
+
+// const imageAddress = "http://erik-crg.dk/croco.jpg";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  console.log("init");
+  document.querySelector("#makePuzzle").addEventListener("click", loadTheImage);
+}
+
+function loadTheImage() {
+  console.log("loadTheImage");
+  // get input image + values
+  inputSrc = document.querySelector("#inputSrc").value;
+  numOfXPieces = document.querySelector("#inputX").value;
+  numOfYPieces = document.querySelector("#inputY").value;
+
+  // load image in DOM
+  document.querySelector("img").src = inputSrc;
+  document.querySelector("img").onload = dropLoop();
+}
+
+// function theImageHasLoaded() {
+//   console.log("theImageHasLoaded");
+//   console.log(inputSrc);
+//   console.log(inputX);
+//   console.log(inputY);
+// }
+
+function dropLoop() {
+  console.log(dropLoop);
+  document.querySelector("#container").style.gridTemplateColumns = `repeat(${numOfXPieces}, 1fr)`;
+  document.querySelector("#container").style.width = `${container_width}px`
+  for (let y = 0; y < numOfYPieces; y++) {
+    for (let x = 0; x < numOfXPieces; x++) {
+
+      let piece = document.createElement("div");
+
+      piece.style.height = (container_height / numOfYPieces) + "px";
+
+      piece.textContent = `${x}${y}`;
+      piece.classList.add("piece");
+
+      document.querySelector("#container").appendChild(piece);
+    }
+  }
+}
+// You find out how big the picture is.
+// You make a 2D loop to generate the dropzones, according to the user input.
+// You give the pieces an ID, need I say it has to be unique.
